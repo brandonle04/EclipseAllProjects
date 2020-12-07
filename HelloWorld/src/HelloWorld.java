1aa70dc0182fc9daead6b5e00721b6cd31d6f527
Brandon Le <Brandon Le@DESKTOP-TSRFLCK>
2020-12-07 14:28:47 -0800

    helloworld push

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+module HelloWorld
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// this is a single line comment
+		// this is another comment
+		
+		/*
+		 * Brandon is so cool
+		 * I love myself very much
+		 * We will get married
+		 */
+		
+		int y = 2;
+		System.out.print(y%=2);
+		
+	}
+
+}
